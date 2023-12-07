25df2058ed282326ff6718b06094c06145ac24c1
Danilo Meneghel <danilo.meneghel@gmail.com>
2023-12-07 20:38:57 -0300

    Atualização de arquivos

--- src/main/java/restaurante/controller/api/ApiCategoriaController.java
+++ src/main/java/restaurante/controller/api/ApiCategoriaController.java
@@ -18,36 +18,36 @@ import java.util.List;
 public class ApiCategoriaController {
 
     @Autowired
-    private CategoriaService classficadorService;
+    private CategoriaService categoriaService;
 
     @GetMapping("/categorias")
     public ResponseEntity<List<Categoria>> mostrarCategorias() {
-        return new ResponseEntity<>(classficadorService.findAllCategorias(), HttpStatus.OK);
+        return new ResponseEntity<>(categoriaService.findAllCategorias(), HttpStatus.OK);
     }
 
     @PostMapping("/categoria/cadastro")
     public ResponseEntity<Categoria> cadastroCategoria(@RequestBody Categoria categoria) {
-        return new ResponseEntity<>(classficadorService.salvarCategoria(categoria), HttpStatus.CREATED);
+        return new ResponseEntity<>(categoriaService.salvarCategoria(categoria), HttpStatus.CREATED);
     }
 
     @PutMapping("/categoria/editar/{id}")
     public ResponseEntity<Categoria> editarCategoria(@PathVariable Long id, @RequestBody Categoria categoria) {
-        Categoria cat = classficadorService.findCategoriaById(id);
+        Categoria cat = categoriaService.findCategoriaById(id);
         if (cat == null) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
         categoria.setId(cat.getId());
-        return new ResponseEntity<>(classficadorService.salvarCategoria(categoria), HttpStatus.OK);
+        return new ResponseEntity<>(categoriaService.salvarCategoria(categoria), HttpStatus.OK);
     }
 
     @DeleteMapping("/categoria/excluir/{id}")
     public void excluirCategoria(@PathVariable Long id) {
-        classficadorService.excluirCategoria(id);
+        categoriaService.excluirCategoria(id);
     }
 
     @GetMapping("/categoria/pesquisa")
     public ResponseEntity<List<Categoria>> pesquisarCategoria(String pesquisa) {
-        return new ResponseEntity<>(classficadorService.findCategoriaByNome(pesquisa), HttpStatus.OK);
+        return new ResponseEntity<>(categoriaService.findCategoriaByNome(pesquisa), HttpStatus.OK);
     }
 
 }
